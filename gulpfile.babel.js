8eccc797fe5f5cb97ecac1347b1e8dccb673ad8b
Kevin Georgy <kevin.georgy@bienair.com>
2016-11-21 15:49:33 +0100

    Handle sass compilation and integrate blade css toolkit.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,10 @@
 
 import gulp from 'gulp';
 import uglify from 'gulp-uglify';
+import sass from 'gulp-sass';
+import cssnano from 'gulp-cssnano';
+import rename from 'gulp-rename';
+import concat from 'gulp-concat';
 import transform from 'vinyl-transform';
 import source from 'vinyl-source-stream';
 import buffer from 'vinyl-buffer';
@@ -11,9 +15,22 @@ import riotify from 'riotify';
 
 const config = {
   sourceDir: 'src',
+  scssDir: 'scss',
   distDir: 'dist',
   entryPoint: 'app'
 }
+
+gulp.task('sass', () => {
+  return gulp.src(config.sourceDir + '/**/*.scss')
+    .pipe(sass())
+
+    //.pipe(rename(function (path) {
+    //  path.extname = '.min.css';
+    //}))
+    .pipe(concat(config.entryPoint + '.min.css'))
+    .pipe(cssnano({autoprefixer: {browsers: 'last 2 versions', add: true}}))
+    .pipe(gulp.dest('./' + config.distDir));
+});
 
 gulp.task('browserify', () => {
   var browserified = function(entry) {
@@ -30,4 +47,4 @@ gulp.task('browserify', () => {
     .pipe(gulp.dest('./' + config.distDir));
 });
 
-gulp.task('default', ['browserify']);
+gulp.task('default', ['browserify', 'sass']);
